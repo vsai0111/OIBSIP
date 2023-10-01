958e78550b5b5f794da186709319bb1e6203b9e5
Vasamshetti sai <139035044+vsai0111@users.noreply.github.com>
2023-10-01 12:37:21 +0530

    update

--- Calculater/caluclator.js
+++ Calculater/caluclator.js
@@ -1,21 +1,22 @@
 let string = "";
-let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=>{
-  button.addEventListener('click', (e)=>{
-    if(e.target.innerHTML == '='){
+let buttons = document.querySelectorAll('button');
+let arr = Array.from(buttons);
+
+arr.forEach(button => {
+  button.addEventListener('click', (e) => {
+    if (e.target.innerHTML == '=') {
       string = eval(string);
-      
+      document.querySelector('input').value = string;
+    } else if (e.target.innerHTML == 'AC') {
+      string = '';
+      document.querySelector('input').value = string;
+    } else if (e.target.dataset.value == '*') {
+      consol.log(e.target.dataset.value)
+      // string = '';
+      // document.querySelector('input').value = string;
+    } else {
+      string = string + e.target.innerHTML;
       document.querySelector('input').value = string;
     }
-    else if(e.target.innerHTML == 'C'){
-      string = ""
-      document.querySelector('input').value = string;
-    }
-    else{ 
-    console.log(e.target)
-    string = string + e.target.innerHTML;
-    console.log(string);
-    document.querySelector('input').value = string;
-      }
   })
 })
